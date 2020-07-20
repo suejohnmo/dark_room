51611c168370cec5f9235bbbbaba6c9f6244ba88
suejohnmo <susan.mwaura@gmail.com>
2020-07-20 23:40:44 +0300

    adding new files to repo

---
+++ app.js
@@ -0,0 +1,15 @@
+// Import express package
+const express = require("express");
+
+// Initialize express
+const app = express();
+
+// Set a static folder
+app.use(express.static("public"));
+
+// Define the port number
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
